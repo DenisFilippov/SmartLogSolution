5ac9463940583b27b96777fb387d8c38da97c543
Denis Filippov <samara_den@mail.ru>
2021-01-02 21:15:18 +0400

    Реализован объект ответа.

---
+++ SmartLog.Domain/Dto/ErrorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace SmartLog.Domain.Dto
+{
+  public class ErrorDto
+  {
+    [XmlElement("UId")]
+    [JsonPropertyName("uid")]
+    public Guid UId { get; set; }
+
+    [XmlElement("Message")]
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+  }
+}
---
+++ SmartLog.Domain/Dto/SmartLogResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace SmartLog.Domain.Dto
+{
+  [XmlRoot(nameof(SmartLogResponse))]
+  public class SmartLogResponse
+  {
+    [XmlElement("Code")]
+    [JsonPropertyName("code")]
+    public int Code { get; set; }
+
+    [XmlElement("Error", IsNullable = false)]
+    [JsonPropertyName("error")]
+    public ErrorDto Error { get; set; }
+  }
+}
